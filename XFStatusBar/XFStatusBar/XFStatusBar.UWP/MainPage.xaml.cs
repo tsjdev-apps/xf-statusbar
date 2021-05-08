5eb54c4a4f0ca7dba14def61e9c4d49ad86fc411
Sebastian Jensen <apps@tsjdev-apps.de>
2021-05-08 21:57:19 +0200

    Add sample for showing how to change the StatusBar on Android, iOS and UWP.

---
+++ XFStatusBar/XFStatusBar/XFStatusBar.UWP/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.ViewManagement;
+using Xamarin.Forms.Platform.UWP;
+
+namespace XFStatusBar.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new XFStatusBar.App());
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = Xamarin.Forms.Color.FromHex("#CC0000").ToWindowsColor();
+        }
+    }
+}
---
+++ XFStatusBar/XFStatusBar/XFStatusBar.iOS/Renderers/CustomPageRenderer.cs
@@ -0,0 +1,67 @@
+﻿using Foundation;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XFStatusBar.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CustomPageRenderer))]
+namespace XFStatusBar.iOS.Renderers
+{
+    public class CustomPageRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+                return;
+
+            try
+            {
+                UpdateStatusBarColor();
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"{GetType().Name} | {nameof(OnElementChanged)} | {ex}");
+            }
+        }
+
+        private void UpdateStatusBarColor()
+        {
+            var statusBar = GetStatusBar();
+            statusBar.BackgroundColor = Color.FromHex("#CC0000").ToUIColor();
+        }
+
+        private UIView GetStatusBar()
+        {
+            UIView statusBar;
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                var customTag = 12456789;
+                var window = UIApplication.SharedApplication.Windows.FirstOrDefault();
+
+                statusBar = window.ViewWithTag(customTag);
+
+                if (statusBar == null)
+                {
+                    statusBar = new UIView(UIApplication.SharedApplication.StatusBarFrame)
+                    {
+                        Tag = customTag
+                    };
+
+                    window.AddSubview(statusBar);
+                }
+            }
+            else
+            {
+                statusBar = UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) as UIView;
+            }
+
+            return statusBar;
+        }
+    }
+}
